259b90277f126bbcb7f0df244ba995985cbde312
uhiriwe <olivineuhiriwe2001@gmail.com>
2024-11-21 11:51:41 +0200

    changes

---
+++ lib/pages/view_map_route.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ViewMapRoute extends StatefulWidget {
+  const ViewMapRoute({super.key});
+
+  @override
+  State<ViewMapRoute> createState() => _ViewMapRouteState();
+}
+
+class _ViewMapRouteState extends State<ViewMapRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
